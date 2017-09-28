daa4e15cf7ac950e7a3304e2a1af0727facc6afe
seke <sekewei@gmail.com>
2017-09-28 23:38:31 +0800

    compile all sources and generate docs

--- build.bat
+++ build.bat
@@ -2,9 +2,18 @@ rem
 rem  build class/*
 rem
 if not exist class md class
-javac -d class -Xlint:unchecked org/openstreetmap/gui/jmapviewer/*.java
+dir /s /B *.java > sources.txt
+javac -d class -Xlint:unchecked @sources.txt 
 
+rem
+rem produce jmap.jar
+rem
 if not exist lib md lib
 cd class
 jar cvfm ../lib/jmap.jar ../manifest.mf .
 cd ..
+
+rem
+rem produce docs/
+rem
+javadoc -d docs @sources.txt
